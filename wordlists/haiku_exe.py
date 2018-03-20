3561cf81aba298555174bb229f6acc437f36b1be
deannagran <dgran@ufl.edu>
2018-03-20 15:36:33 -0400

    updated haiku gen script

--- wordlists/haiku_exe.py
+++ wordlists/haiku_exe.py
@@ -13,30 +13,43 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api = tweepy.API(auth)
 
 i = 0
-
-firstadjective = random.choice(open("AT-1.txt").readlines()).rstrip()
-firstnoun = random.choice(open("NN-1.txt").readlines()).rstrip()
-verb = random.choice(open("VBZ-1.txt").readlines()).rstrip()
-secondadjective = random.choice(open("AT-1.txt").readlines()).rstrip()
-secondnoun = random.choice(open("NN-1.txt").readlines()).rstrip()
-
-if firstadjective == "a" and (firstnoun[0] == 'a' or firstnoun[0] == 'e' or firstnoun[0] == 'i' or firstnoun[0] == 'o' or firstnoun[0] == 'u'):
-    firstadjective = "an"
-
-if firstadjective == "an" and not((firstnoun[0] == 'a') or (firstnoun[0] == 'e') or (firstnoun[0] == 'i') or (firstnoun[0] == 'o') or (firstnoun[0] == 'u')):
-    firstadjective = "a"
-
-if secondadjective == "a" and  (secondnoun[0] == 'a' or secondnoun[0] == 'e' or secondnoun[0] == 'i' or secondnoun[0] == 'o' or secondnoun[0] == 'u'):
-    secondadjective = "an"
-
-if secondadjective == "an" and not(secondnoun[0] == 'a' or secondnoun[0] == 'e' or secondnoun[0] == 'i' or secondnoun[0] == 'o' or secondnoun[0] == 'u'):
-    secondadjective = "a"
-
-finalstring = firstadjective + " " + firstnoun + " " + verb + " " + secondadjective + " " + secondnoun
-
 while True:
-    api.update_status(finalstring)
+
+    firstadjective = random.choice(open("AT-1.txt").readlines()).rstrip()
+    firstnoun = random.choice(open("NN-1.txt").readlines()).rstrip()
+    verb = random.choice(open("VBZ-1.txt").readlines()).rstrip()
+    secondadjective = random.choice(open("AT-1.txt").readlines()).rstrip()
+    secondnoun = random.choice(open("NN-1.txt").readlines()).rstrip()
+
+    firstVerb = random.choice(open("VB-3.txt").readlines()).rstrip()
+    secondVerb = random.choice(open("VB-3.txt").readlines()).rstrip()
+    thirdVerb = random.choice(open("VB-1.txt").readlines()).rstrip()
+
+    lastLineAdj = random.choice(open("JJ-2.txt").readlines()).rstrip()
+
+
+    if firstadjective == "a" and (firstnoun[0] == 'a' or firstnoun[0] == 'e' or firstnoun[0] == 'i' or firstnoun[0] == 'o' or firstnoun[0] == 'u'):
+        firstadjective = "an"
+
+    if firstadjective == "an" and not((firstnoun[0] == 'a') or (firstnoun[0] == 'e') or (firstnoun[0] == 'i') or (firstnoun[0] == 'o') or (firstnoun[0] == 'u')):
+        firstadjective = "a"
+
+    if secondadjective == "a" and  (secondnoun[0] == 'a' or secondnoun[0] == 'e' or secondnoun[0] == 'i' or secondnoun[0] == 'o' or secondnoun[0] == 'u'):
+        secondadjective = "an"
+
+    if secondadjective == "an" and not(secondnoun[0] == 'a' or secondnoun[0] == 'e' or secondnoun[0] == 'i' or secondnoun[0] == 'o' or secondnoun[0] == 'u'):
+        secondadjective = "a"
+
+    firstLine = firstadjective + " " + firstnoun + " " + verb + " " + secondadjective + " " + secondnoun
+    middleLine = firstVerb + ", " + secondVerb + ", " + thirdVerb
+    lastLine = firstadjective + " " + firstnoun + " is " + lastLineAdj + "."
+
+
+    api.update_status(firstLine +"\n" + middleLine + "\n" + lastLine)
     print("Tweeting!")
     i = i + 1
     time.sleep(90) #Tweet every 2 minutes
     print("All done tweeting!")
+
+    #python haiku_exe.py
+    #cd C:\Users\ddgra\Desktop\haiku.exe\wordlists

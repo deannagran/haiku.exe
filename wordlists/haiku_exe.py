5c3c1a4484cea1c8fe301106427765fdedf8d725
Julian <frardvark@gmail.com>
2018-04-14 21:27:16 -0400

    adfsfasd

--- wordlists/haiku_exe.py
+++ wordlists/haiku_exe.py
@@ -57,6 +57,104 @@ def make5line():
         haikuline = a3s2p()
     return haikuline
 
+def make7line():
+    rand = random.randint(1, 100)
+    if (rand < 20):
+        haikuline = a5s2p()
+    elif (rand < 40):
+        haikuline = a4s3p()
+    elif (rand < 60):
+        haikuline = a3s4p()
+    elif (rand < 80):
+        haikuline = a2s5p()
+    else:
+        haikuline = a1s6p()
+    return haikuline
+    
+def a5s2p():
+    noun = a5n()
+    predicate = a1v() + ' ' + a1n()
+    line = noun + ' ' + predicate
+    return line
+
+def a4s3p():
+    noun = a4n()
+    rand = random.randint(1, 2)
+    if (rand == 1):
+        predicate = a1v() + ' ' + a2n()
+    else:
+        predicate = a2v() + ' ' + a1n()
+    line = noun + ' ' + predicate
+    return line
+
+def a3s4p():
+    noun = a3n()
+    rand = random.randint(1, 3)
+    if (rand == 1):
+        predicate = a1v() + ' ' + a3n()
+    elif (rand == 2):
+        predicate = a2v() + ' ' + a2n()
+    else:
+        predicate = a1v() + ' ' + a3n()
+    line = noun + ' ' + predicate
+    return line
+
+def a2s5p():
+    noun = a2n()
+    rand = random.randint(1, 4)
+    if (rand == 1):
+        predicate = a1v() + ' ' + a4n()
+    elif (rand == 2):
+        predicate = a2v() + ' ' + a3n()
+    elif (rand == 3):
+        predicate = a3v() + ' ' + a2n()
+    else:
+        predicate = a4v() + ' ' + a1n()
+    line = noun + ' ' + predicate
+    return line
+
+def a1s6p():
+    noun = a1n()
+    rand = random.randint(1, 5)
+    if (rand == 1):
+        predicate = a1v() + ' ' + a5n()
+    elif (rand == 2):
+        predicate = a2v() + ' ' + a4n()
+    elif (rand == 3):
+        predicate = a3v() + ' ' + a3n()
+    elif (rand == 4):
+        predicate = a4v() + ' ' + a2n()
+    else:
+        predicate = a5v() + ' ' + a1n()
+    line = noun + ' ' + predicate
+    return line
+
+def a5n():
+    rand = random.randint(1, 5)
+    if (rand == 1):
+        string = a1a() + ' ' + a4n()
+    elif (rand == 2):
+        string = a2a() + ' ' + a3n()
+    elif (rand == 3):
+        string = a3a() + ' ' + a2n()
+    elif (rand == 4):
+        string = a4a() + ' ' + a1n()
+    else:
+        string = random.choice(open("NN-5.txt").readlines()).rstrip()
+    return string
+
+def a4n():
+    rand = random.randint(1, 4)
+    if (rand == 1):
+        string = a1a() + ' ' + a3n()
+    elif (rand == 2):
+        string = a2a() + ' ' + a2n()
+    elif (rand == 3):
+        string = a3a() + ' ' + a1n()
+    else:
+        string = random.choice(open("NN-4.txt").readlines()).rstrip()
+    return string
+    
 
 def a1s4p():
     noun = a1n()
@@ -78,13 +176,13 @@ def a2s3p():
         predicate = a2v() + ' ' + a1n()
     elif (rand == 2):
         predicate = a1v() + ' ' + a2n()
-        line = noun + ' ' + predicate
+    line = noun + ' ' + predicate
     return line
 
 
 def a3s2p():
     noun = a3n()
-    predicate = a1v() + a1n()
+    predicate = a1v() +  ' ' + a1n()
     line = noun + ' ' + predicate
     return line
 
@@ -129,7 +227,15 @@ def a3v():
         string = a2av() + ' ' + a1v()
     else:
         string = random.choice(open("VBZ-3.txt").readlines()).rstrip()
-        return string
+    return string
+
+def a4v():
+    rand = random.randint(1, 2)
+    if (rand == 1):
+        string = a2av() + ' ' + a2v()
+    else:
+        string = random.choice(open("VBZ-4.txt").readlines()).rstrip()
+    return string
 
 
 def a1a():
@@ -150,6 +256,24 @@ def a2a():
             open("JJ-1.txt").readlines()).rstrip()
     return adj
 
+def a3a():
+    rand = random.randint(1, 2)
+    if (rand == 1):
+        adj = random.choice(open("JJ-3.txt").readlines()).rstrip()
+    else:
+        adj = random.choice(open("AT-1.txt").readlines()).rstrip() + " " + random.choice(
+            open("JJ-2.txt").readlines()).rstrip()
+    return adj
+
+def a4a():
+    rand = random.randint(1, 2)
+    if (rand == 1):
+        adj = random.choice(open("JJ-4.txt").readlines()).rstrip()
+    else:
+        adj = random.choice(open("AT-1.txt").readlines()).rstrip() + " " + random.choice(
+            open("JJ-3.txt").readlines()).rstrip()
+    return adj
+
 
 def a2av():
     adv = random.choice(open("RB-2.txt").readlines()).rstrip()
@@ -158,11 +282,12 @@ def a2av():
 while True:
     firstLine = make5line()
     print(firstLine)
-    # middleLine = make7line() worry about this shit later
+    middleLine = make7line()
+    print(middleLine)
     lastLine = make5line()
     print(lastLine)
 
-    # api.update_status(firstLine +"\n" + "\n" + lastLine)
+    api.update_status(firstLine +"\n" + middleLine + "\n" + lastLine)
     print("Tweeting!")
     i = i + 1
     time.sleep(90)  # Tweet every 2 minutes
